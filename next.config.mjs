06c635a6657fd3c1986634517d2604a02fbe1f8a
laucorosan-spec <laucorosan@gmail.com>
2026-05-25 12:56:35 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Aquí añadimos una línea
-  experimental: {
-    appDir: true, // Ya debería estar si creaste el proyecto con App Router
-  },
-  // Importante: esta línea ayuda a Next.js con CSS
-  reactStrictMode: true, 
-}
+const nextConfig = {};
 
-module.exports = nextConfig
+export default nextConfig;
