589b6b6d74367ae5643744935470dc5cde53af59
laucorosan-spec <laucorosan@gmail.com>
2026-05-26 16:34:42 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,12 +4,15 @@ import Navbar from '@/components/Navbar';
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className="bg-[#fdfbf7]">
-        {/* Este 'max-w-md mx-auto' hace que parezca una App móvil en PC */}
-        <main className="max-w-md mx-auto min-h-screen px-6 pb-32 pt-8 relative">
-          {children}
-        </main>
-        <Navbar />
+      <body className="bg-[#fdfbf7] antialiased">
+        {/* Contenedor principal que centra la app en PC */}
+        <div className="max-w-md mx-auto min-h-screen relative shadow-sm bg-[#fdfbf7]">
+          <main className="px-6 pt-8 pb-32">
+            {children}
+          </main>
+          {/* El Navbar va AQUÍ adentro para que se mueva con la App */}
+          <Navbar />
+        </div>
       </body>
     </html>
   );
