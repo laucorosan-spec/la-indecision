bc8f6483752716ab3de14f0dfcc0214684ba45eb
laucorosan-spec <laucorosan@gmail.com>
2026-05-26 16:29:04 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,12 @@
 import './globals.css';
-import Navbar from '../components/Navbar';
-
-export const metadata = {
-  title: 'La Indecisión',
-  description: '¿Qué hacemos hoy?',
-};
+import Navbar from '@/components/Navbar';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className="pb-24">
-        <main className="max-w-md mx-auto p-4 pt-8">
+      <body className="bg-[#fdfbf7]">
+        {/* Este 'max-w-md mx-auto' hace que parezca una App móvil en PC */}
+        <main className="max-w-md mx-auto min-h-screen px-6 pb-32 pt-8 relative">
           {children}
         </main>
         <Navbar />
